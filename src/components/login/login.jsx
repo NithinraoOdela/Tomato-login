99ac75d65221d70ad37dc4b718e386c6e2990e19
NithinraoOdela <65495762+NithinraoOdela@users.noreply.github.com>
2021-08-22 20:20:24 +0530

    Add files via upload

---
+++ src/components/login/login.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import loginImg from "../../login.svg";
+
+
+
+
+export class Login extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    handlerclick = () => {
+        alert('Eat Sleep Binge')
+
+        window.open('https://nithinraoodela.github.io/Registration/', '_self')
+        
+        
+        
+
+
+
+       
+
+
+         
+
+
+
+    }
+
+
+    render() {
+        return (
+            <form action="/" method="GET">
+                <div className="base-container" ref={this.props.containerRef}>
+                <h1 > <strong className="imagee">Eat</strong>  <strong className="imagew">Sleep</strong> <strong className="imageq">Binge</strong></h1>
+                    <div className="header">Login</div>
+                    <div className="content">
+                        <div className="image">
+                            <img src={loginImg} />
+                        </div>
+                        <div className="form">
+                            <div className="form-group">
+                                <label htmlFor="User Name">User Name</label>
+                                <input type="text" name="User Name" id="isd" placeholder="User Name" ref={this.inputRef}required/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                <input type="password"id="isdd" name="password" placeholder="password" required />
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="footer2">
+                        <button type="button" className="btn"  id="btt" onClick={this.handlerclick} > Login
+
+                        </button>
+
+                    </div>
+                </div>
+
+            </form>
+        );
+    }
+}
---
+++ src/components/login/register.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import loginImg from "../../login.svg";
+
+export class Register extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handlerclick = () => {
+
+        
+        var un = document.getElementById("isd").value
+        if (un ==="") {
+            
+            alert("Blank / Invalid ID")
+            window.open('', '_self')
+        }
+        else{
+            
+            alert("Registerd")
+            window.open('login.jsx', '_self')
+        }
+
+
+    }
+
+
+    render() {
+        return (
+            <form>
+                <div className="base-container" ref={this.props.containerRef}>
+                <h1 > <strong className="imagee">Eat</strong>  <strong className="imagew">Sleep</strong> <strong className="imageq">Binge</strong></h1>
+                    <div className="header">Register</div>
+                    <div className="content">
+                        <div className="image">
+                            <img src={loginImg} />
+                        </div>
+                        <div className="form">
+                            <div className="form-group">
+                                <label htmlFor="User Name">User Name</label>
+                                <input type="text" name="User Name" id="isd" placeholder="User Name" required  />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="email">Email</label>
+                                <input type="email" name="email" placeholder="email" required />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                <input type="password" name="password" placeholder="password" required />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="footer1">
+                        <button type="button" className="btn"  onClick={this.handlerclick} > Register
+
+                        </button>
+                    </div>
+                </div>
+            </form>
+        );
+        
+    }
+}
